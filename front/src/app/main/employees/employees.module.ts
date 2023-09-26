e3068a244c52d4fa303997298b37e3d963ec7292
HAROUN <haroundominique@gmail.com>
2023-09-26 13:17:52 +0200

    progressing on the tutorial

--- front/src/app/main/employees/employees.module.ts
+++ front/src/app/main/employees/employees.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { EmployeesRoutingModule } from './employees-routing.module';
-
+import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { EmployeesHomeComponent } from './employees-home/employees-home.component';
+import { EmployeesDetailComponent } from './employees-detail/employees-detail.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [EmployeesHomeComponent, EmployeesDetailComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
